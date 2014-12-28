a4737238911390003d66cf793f749f729644115f
deviozc <deviozc@gmail.com>
2014-12-28 05:48:39 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,46 @@
+/**
+ * Created with lavamelon-sails-cms-core.
+ * User: deviozc
+ * Date: 2014-12-28
+ * Time: 04:27 AM
+ * To change this template use Tools | Templates.
+ */
+var MongoClient = require('mongodb').MongoClient;
+(function() {
+    var geocoderProvider = 'google';
+    var httpAdapter = 'http';
+    var geocoder = require('node-geocoder').getGeocoder(geocoderProvider, httpAdapter);
+    MongoClient.connect('mongodb://127.0.0.1:27017/sails', function(err, db) {
+        var collection = db.collection('property');
+        collection.find({
+            status: 'imported'
+        }).limit(50).toArray(function(err, properties) {
+            if(err){
+                console.log(err);
+            }
+            var i = 0;
+            (function loop() {
+                var property = properties[i];
+                var address = property.fullAddress + ", " + property.city + ", BC, Canada";
+                geocoder.geocode(address, function(err, res) {
+                    collection.update({
+                        _id: property._id
+                    }, {
+                        $set: {
+                            status: 'processed',
+                            longitude: res[0].longitude,
+                            latitude: res[0].latitude
+                        }
+                    }, function(err, result){
+                        console.log(result);
+                    });
+                });
+                if(++i < properties.length) {
+                    setTimeout(loop, 250); // call myself in 2 seconds time if required
+                }else{
+                    db.close();
+                }
+            })();
+        });
+    });
+})();
